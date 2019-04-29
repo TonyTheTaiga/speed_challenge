1a1f4147ace349ef22d0dee05a410b47131aa47d
Taiga Ishida <taiga@Taigas-Mac-mini.local>
2019-04-28 18:03:38 -0700

    ayye

--- optical.py
+++ optical.py
@@ -3,20 +3,20 @@ import cv2
 import numpy as np
 import random
 
-def adjustGamma(image, gamma=1.0):
+def adjustGamma(imagnitudee, gamma=1.0):
     invGamma = 1.0 / gamma
     table = np.array([((i/255.0) ** invGamma) * 255
         for i in np.arange(0,256)]).astype("uint8")
-    return cv2.LUT(image.astype(np.uint8), table.astype(np.uint8))
+    return cv2.LUT(imagnitudee.astype(np.uint8), table.astype(np.uint8))
 
 def main():
     cap = cv2.VideoCapture('speed_data/train.mp4')
 
     ret, base_frame = cap.read()
-    base_gray = cv2.cvtColor(base_frame, cv2.COLOR_BGR2GRAY)
+    base_gray = cv2.cvtColor(adjustGamma(base_frame, random.uniform(1.0, 1.5)),cv2.COLOR_BGR2GRAY)
     hsv = np.zeros_like(base_frame) 
     hsv[...,1] = 255
-
+    #[hue, saturation, value]
     tl = open('speed_data/train.txt', 'r')
     i = 1
     
@@ -30,26 +30,32 @@ def main():
             break
 
         gammaVal = random.uniform(1.0, 1.5)
-
         next_gray = cv2.cvtColor(adjustGamma(next_frame, gammaVal),cv2.COLOR_BGR2GRAY)
+
+        print(f'frame:{i}\nlabel:{tl.readline().strip()}\nGamma Val:{gammaVal}\n')
 
         flow = cv2.calcOpticalFlowFarneback(base_gray,
             next_gray, flow=None, pyr_scale=0.5,
             levels=3, winsize=15, iterations=3,
             poly_n=5, poly_sigma=1.2, flags=0)
 
-        print(f'frame:{i}\nlabel:{tl.readline().strip()}\nGamma Val:{gammaVal}\n')
+        magnitude, angle = cv2.cartToPolar(flow[...,0], flow[...,1])
 
-        mag, ang = cv2.cartToPolar(flow[...,0], flow[...,1])
+        print(magnitude[180])
 
-        hsv[...,0] = ang*180/np.pi/2
-        hsv[...,2] = cv2.normalize(src=mag, dst=None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
+        hsv[...,0] = angle*180/np.pi/2
+        hsv[...,2] = cv2.normalize(src=magnitude, dst=None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
+
+        print(hsv[...,2][180])
+
 
         rgb = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
 
+        cv2.imshow(f'previous frame', base_gray[50:360])
         cv2.imshow(f'current frame', next_gray[50:360])
         cv2.imshow('opticalflow',rgb[50:360])
-        cv2.moveWindow('current frame', 0, 0)
+        cv2.moveWindow('previous frame', 0, 0)
+        cv2.moveWindow('current frame', 680, 0)
         cv2.moveWindow('opticalflow', 0, 400)
         #cv2.imwrite(f'opFlow.nosync/opflow_{i}.png', rgb[50:360])
 
